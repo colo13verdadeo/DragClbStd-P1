5b76895d23b978392f4f620ca345ffe1849c4e75
rodrigo745 <64508573+rodrigo745@users.noreply.github.com>
2020-11-23 17:57:11 -0300

    Add files via upload

---
+++ Dragon Studio/js/languajes.js
@@ -0,0 +1,21 @@
+$.getJSON('js/lang.json', function(json){
+
+    if(!localStorage.getItem('lang')){
+        localStorage.setItem('lang', "en");
+    }
+    let def = localStorage.getItem("lang");
+    $('.lang').each(function(index,value){
+        $(this).text(json[def][$(this).attr('key')]);
+    });
+
+    $('.translate').click(function(){
+        let lang = $(this).attr("id");
+        localStorage.setItem("lang", lang)
+        $('.lang').each(function(index,value){
+            $(this).text(json[lang][$(this).attr('key')]);
+        })
+    });
+});
+
+
+
